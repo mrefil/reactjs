a414a0004bfc476771165f433a4cf60f6403c4b3
mrefil <muefiloglu@protonmail.com>
2021-03-29 13:30:46 +0300

    add Json

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,21 +6,14 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Muro',
-          id: 'adc1'
-        },
-        {
-          name: 'Salako',
-          id: 'adc2'
-        },
-        {
-          name: 'Maho',
-          id: 'adc3'
-        }
-      ]
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }));
   }
 
   render() {
